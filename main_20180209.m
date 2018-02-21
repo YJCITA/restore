6c948bae6cfd8393ed387387392039e93fcf0fdc
YJ <yujinwpu@163.com>
2018-02-21 23:03:10 +0800

    tmp save

--- main_20180209.m
+++ main_20180209.m
@@ -10,7 +10,7 @@ clear all
 close all
 
 data_raw_tmp = load('./data/11/data.txt')';
-data_raw = data_raw_tmp(:, 1:2000);
+data_raw = data_raw_tmp; %(:, 1:7000);
 data_raw(1, :) = data_raw(1, :)/1e6;  %将us转换为s;
 data_raw(3, :) = -data_raw(3, :); % 为方便画图，图像的坐标系转换
 
@@ -28,30 +28,47 @@ j_spreate = 0; % 代表分离的笔划
 i_count = 0;
 time_base = 0; % 因为目前时间戳不是从一开机就连续增加的，而是每次检测到压力开机后才开始计时，所以为了分析方便，做累加
 data_length = length(timestamp_raw);
+time_pre = 0;
 for i = 1:data_length
-    if xy_state_raw(i) == 1 || pressure_raw(i) == 0 % 只有当图像解算坐标值有效
-        if pressure_raw(i) == 1 
-            i_count = i_count + 1; % 一个连划里面的点计数
-            data_spreate_tmp(:, i_count) = data_raw(:, i);
-            data_spreate_tmp(1, i_count) = data_spreate_tmp(1, i_count) + time_base; % 累加时间
-        else
-            if i_count > 0
-                j_spreate = j_spreate + 1; % 新的一个连划
-                data_spreate{j_spreate} = data_spreate_tmp;
-                time_base = time_base + data_raw(1, i);
-                clear data_spreate_tmp;
-                i_count = 0;
-            end
-        end
+    xy_state_cur = xy_state_raw(i);
+    pressure_cur = pressure_raw(i);
+    data_cur = data_raw(:, i);
+    if i == 145
+        test = 1;
     end
+    
+    if xy_state_cur == 1 && pressure_cur == 1 % 只有当图像解算坐标值有效,并且有压力值时才记为有效数据
+        % 说明数据从无效重新变为有效(有可能是)
+        if i_count == 0
+            time_base = time_base + time_pre;
+            clear data_spreate_tmp;
+        end
+        i_count = i_count + 1; % 一个连划里面的点计数
+        data_spreate_tmp(:, i_count) = data_cur;
+        data_spreate_tmp(1, i_count) = data_spreate_tmp(1, i_count) + time_base; % 累加时间
+        
+    end
+    
+    % 当压力值变为0，同时累积的数据大于0个，则存储
+    if pressure_cur == 0 && i_count > 0
+        j_spreate = j_spreate + 1; % 新的一个连划
+        data_spreate{j_spreate} = data_spreate_tmp;
+        time_pre = data_cur(1);
+        i_count = 0;
+    end
+    
+
 end
 
-%% 根据速度剔除异常值，新的有效数据都在new_data中
-% 新数据new_data =[time xy*2 vxy*2 is_new_trace(是否是新的连笔)]
+%% 根据速度剔除异常值，新的有效数据都在data_new中
+% 新数据data_new =[time xy*2 vxy*2 is_new_trace(是否是新的连笔)]
 i_save = 0;
-j_new_data = 0; % 重构数据，剔除异常值
-% new_data; % 新的数据
+j_data_new = 0; % 重构数据，剔除异常值
+% data_new; % 新的数据
 for i=1:j_spreate
+    if i == 4
+        test = 1;
+    end
     data_tmp = data_spreate{i};
     [t, data_length1] = size(data_tmp);
     is_first_data = true;
@@ -65,53 +82,79 @@ for i=1:j_spreate
             is_first_data = false;
             
             % 保存新连笔的第一个数据
-            j_new_data = j_new_data + 1;
+            j_data_new = j_data_new + 1;
             % 初始第一个数据速度为0
-            new_data(:, j_new_data) = [time_cur, xy_cur(1), xy_cur(2), 0, 0, 1]';
+            data_new(:, j_data_new) = [time_cur, xy_cur(1), xy_cur(2), 0, 0, 1]';
         else
             dt = time_cur - time_pre;
-            if dt > 0
+            if dt > 0.2
+                test = 1;
+            end
             dxy = xy_cur - xy_pre;
-            if dt > 0
+            dxy_normal = sqrt(dxy(1)^2 + dxy(2)^2);
+            % dt太大的数据直接抛弃
+            % TODO：因为有些时候可能是第一个数据是错的，所以才需要回朔删除第一个数据，将第二个数据当做第一个数据的备选
+            if dt > 0 && dt < 0.2 
                 v_xy = dxy/dt; 
                 v_normal = sqrt(v_xy(1)^2 + v_xy(2)^2); % 速度模值
                 % ---TODO---
                 % 书写速度的阈值，这个可以先这么简单处理，更合理是考虑历史速度，剔除突变
                 if v_normal < 1e5 
                     time_pre = time_cur;
-                    j_new_data = j_new_data + 1;
-                    % 剔除异常值后，所有有效数据都在new_data中
-                    new_data(:, j_new_data) = [time_cur, xy_cur(1), xy_cur(2), v_xy(1), v_xy(2), 0]';
+                    j_data_new = j_data_new + 1;
+                    % 剔除异常值后，所有有效数据都在data_new中
+                    data_new(:, j_data_new) = [time_cur, xy_cur(1), xy_cur(2), v_xy(1), v_xy(2), 0]';
             
                     i_save = i_save + 1;
                     save_v_xy(:, i_save) = [time_cur, v_xy(1), v_xy(2)]';
                     save_v(:, i_save) = [time_cur, v_normal]';
                     save_xy(:, i_save) = [time_cur, xy_cur(1), xy_cur(2)]';
+                    save_dxy_nor(:, i_save) = [time_cur, dxy_normal]';
                     save_dt(:, i_save) = [time_cur, dt]';
                 end
             else
-                printf('error dt = %f\n', dt);
+                time_pre = time_cur;
+%                 printf('error dt = %f\n', dt);
             end
         end
     end
 end
 
 %% 笔迹还原
-% new_data =[time xy*2 vxy*2 is_new_trace(是否是新的连笔)]
-% [t, length_new] = size(new_data);
-% for i = 1:length_new
-%     time_cur = new_data(1, i);
-%     xy_cur = new_data(2:3, i);
-%     Vxy_cur = new_data(4:5, i);
-%     is_new_trace = new_data(6, i);
-%     if is_new_trace == 1
-%         time_pre = time_cur;
-%         xy_pre = xy_cur;
-%         Vxy_pre = Vxy_cur;        
-%     else
-%         dt = time_cur - pre
-%     end
-% end
+% data_new =[time xy*2 vxy*2 is_new_trace(是否是新的连笔)]
+% 直接用速度做插值
+[t, length_new] = size(data_new);
+i_save = 0;
+for i = 1:length_new
+    time_cur = data_new(1, i);
+    xy_cur = data_new(2:3, i);
+    Vxy_cur = data_new(4:5, i);
+    is_new_trace = data_new(6, i);
+    if is_new_trace == 1
+        time_pre = time_cur;
+        xy_pre = xy_cur;
+        Vxy_pre = Vxy_cur;      
+        
+        % 保存数据
+        i_save = i_save + 1;
+        data_restore(:, i_save) = [time_cur, xy_cur(1), xy_cur(2), 1]';
+    else
+        dt = time_cur - time_pre;
+        % 插值
+        dt_insert = 1/800; % 400hz
+        for j = 1 : dt/dt_insert % 400hz
+            dt1 = j*dt_insert;
+            Vxy_cur = (xy_cur - xy_pre)/dt;
+            xy_insert = xy_pre + Vxy_cur*dt1;
+            time_cur_new = time_pre + dt1;
+            i_save = i_save + 1;
+            data_restore(:, i_save) = [time_cur_new, xy_insert(1), xy_insert(2), 0]';
+        end
+        time_pre = time_cur;
+        xy_pre = xy_cur;
+        Vxy_pre = Vxy_cur; 
+    end
+end
 
 
 
@@ -121,8 +164,14 @@ hold on;
 grid on;
 plot(save_v_xy(1,:), save_v_xy(2,:), '.r'); % vx
 plot(save_v_xy(1,:), save_v_xy(3,:), '*'); % vy
-plot(save_v(1,:), save_v(2,:), '-k'); % v
+plot(save_v(1,:), save_v(2,:)); % v
 legend('vx', 'vy', 'v');
+
+figure()
+hold on;
+grid on;
+plot(save_dxy_nor(1,:), save_dxy_nor(2,:), '.r'); % dxy_nor
+legend('dxy-nor');
 
 figure()
 hold on;
@@ -134,17 +183,24 @@ legend('x', 'y');
 figure()
 hold on;
 grid on;
-plot(save_xy(2,:), save_xy(3,:), '.'); % xy
-xlim([3000, 9000]);
-ylim([-5000, -2000]);
-legend('剔除野值后xy');
-
+plot(data_new(2,:), data_new(3,:), '.'); % xy
+% xlim([3000, 9000]);
+% ylim([-5000, -2000]);
+legend('new-xy');
 
 figure()
 hold on;
 grid on;
-plot(save_dt(1,:), save_dt(2,:), '.'); % dt
-legend('dt');
+plot(data_restore(2,:), data_restore(3,:), '.'); % xy-restore
+% xlim([3000, 9000]);
+% ylim([-5000, -2000]);
+legend('xy-restore');
+
+% figure()
+% hold on;
+% grid on;
+% plot(save_dt(1,:), save_dt(2,:), '.'); % dt
+% legend('dt');
 
 % 统计直方图
 % figure()
@@ -202,8 +258,8 @@ legend('dt');
 figure()
 plot(xy_raw(1,:), xy_raw(2,:), '.');
 grid on;
-xlim([3000, 9000]);
-ylim([-5000, -2000]);
+% xlim([3000, 9000]);
+% ylim([-5000, -2000]);
 title('raw-data')
 
 
